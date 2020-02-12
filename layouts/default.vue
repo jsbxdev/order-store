d2c992171b54a5162aec30df281d4b82f1bcc123
Eduardo Barros <joseeduardobarros@gmail.com>
2020-02-12 13:42:26 -0300

    Init Nuxt config

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer v-model="drawer" fixed app> </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" elevate-on-scroll app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
